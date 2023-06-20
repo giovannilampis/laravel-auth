eb59070b660594acb50abbadfc30272a0e0778cc
giovannilampis <giolampis@hotmail.com>
2023-06-20 06:16:00 +0200

    add prefix & name to Routes::middleware in web.php

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
 });
 
 // localhost:8000/admin/----
