824164ed07054de9bad13c7ad73592b01cbadc23
giovannilampis <giolampis@hotmail.com>
2023-06-21 15:52:18 +0200

    made migration

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -9,7 +9,7 @@
 
             <div class="col-3">
                 <div class="card" style=>
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="..." class="card-img-top" alt="">
                     <div class="card-body">
                       <h5 class="card-title">Card title</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
