4eac93c8195381a4799c9bb369e8af70fbaed6f4
giovannilampis <giolampis@hotmail.com>
2023-06-21 04:26:27 +0200

    add projects to admin dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -47,6 +47,10 @@
                     </li>
                     @endif
                     @else
+                    <li class="nav-item">
+                        {{-- <a href="{{ route('admin.projects.index') }}" class="nav-link">Projects</a> --}}
+                        <a href="" class="nav-link">Projects</a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }}
